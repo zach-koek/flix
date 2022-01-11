c31f8d3ffb2d6e0ab8d29b9176436e360dfe5789
CODE_TUNA <Zkoekenberg@hotmail.com>
2022-01-11 11:07:28 -0500

    Added Review Form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,34 @@
 class ReviewsController < ApplicationController
 
+    before_action :set_up
+
     def index
-        @movie = Movie.find(params[:movie_id])
         @reviews = @movie.reviews
     end
 
+    def new
+        @review = @movie.reviews.new
+    end
+
+    def create
+        @review = @movie.reviews.new(review_params)
+
+        if @review.save
+            redirect_to movie_reviews_path(@movie), notice: "Review Successfully Submitted!"
+        else 
+            render :new 
+        end
+    end
+
+
+
+private
+
+    def set_up
+        @movie = Movie.find(params[:movie_id])
+    end
+
+    def review_params
+        params.require(:review).permit(:name, :comment, :stars)
+    end
 end
