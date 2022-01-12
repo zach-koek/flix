76ec6a117c6aa832c29aac6ec8a489657b4c3c28
CODE_TUNA <Zkoekenberg@hotmail.com>
2022-01-12 11:11:36 -0500

    Added name to header after login.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,8 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to @user, notice: "Account Created!"
+            session[:user_id] = @user.id
+            redirect_to @user, notice: "Thank you for signing up!!"
         else
             render :new
         end
