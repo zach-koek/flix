855d6b269868c3e61fd02a9d3e27505f9187f4ca
CODE_TUNA <Zkoekenberg@hotmail.com>
2022-01-14 13:51:07 -0500

    Added "Fave" button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,28 @@
 class FavoritesController < ApplicationController
+
+    before_action :require_signin
+    before_action :set_movie
+    
+    def create
+        @movie.favorites.create!(user: current_user)
+
+        redirect_to @movie
+    end  
+
+    def destroy
+       favorite = current_user.favorites.find_by(params[:id])
+       favorite.destroy
+
+       redirect_to @movie
+
+
+
+    end
+
+    private 
+
+    def set_movie
+        @movie = Movie.find(params[:movie_id])
+    end
+
 end
