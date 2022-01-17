2a35c2dde28e05439a15b8ddb07b250215c4dbbf
CODE_TUNA <Zkoekenberg@hotmail.com>
2022-01-17 09:59:39 -0500

    Genre Checkboxes Added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,7 @@ class MoviesController < ApplicationController
     def show
         @movie= Movie.find(params[:id])
         @fans = @movie.fans 
+        @genres = @movie.genre.order(:name)
         if current_user
             @favorite = current_user.favorites.find_by(movie_id: @movie.id)
         end
@@ -53,7 +54,7 @@ class MoviesController < ApplicationController
 
     def movie_params
         params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross, 
-                                        :director, :duration, :image_file_name)
+                                        :director, :duration, :image_file_name, genre_ids: [])
     end
 
 
