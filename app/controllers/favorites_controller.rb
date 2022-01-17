d1d4c59a5c3c9d9848818e86940c510f9e782020
Zach Koekenberg <Zkoekenberg@hotmail.com>
2022-01-17 14:00:17 -0500

    Fixed bug for path.

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -22,7 +22,7 @@ class FavoritesController < ApplicationController
     private 
 
     def set_movie
-        @movie = Movie.find(params[:movie_id])
+        @movie= Movie.find_by!(slug: params[:movie_id])
     end
 
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -27,7 +27,7 @@ class ReviewsController < ApplicationController
 private
 
     def set_up
-        @movie = Movie.find(params[:movie_id])
+        @movie= Movie.find_by!(slug: params[:movie_id])
     end
 
     def review_params
